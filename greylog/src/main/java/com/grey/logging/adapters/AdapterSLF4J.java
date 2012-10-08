be0c69fb9810316353e15e1ad39c93b0c696f4a8
ybadri <yb@greybox.demon.co.uk>
2012-10-09 00:09:35 +0100

    Adding the new files - just the SLF4J and JCL adapters

---
+++ greylog/src/main/java/com/grey/logging/adapters/AdapterSLF4J.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2010-2012 Yusef Badri - All rights reserved.
+ * NAF is distributed under the terms of the GNU Affero General Public License, Version 3 (AGPLv3).
+ */
+package com.grey.logging.adapters;
+
+import com.grey.base.utils.ScheduledTime;
+
+public class AdapterSLF4J
+	extends com.grey.logging.Logger
+{
+	private final org.slf4j.Logger extlog;  //the external logger we're bridging to
+
+	protected AdapterSLF4J(com.grey.logging.Parameters params, String logname) throws java.io.IOException
+	{
+		super(adjust(params), logname, false);
+		extlog = org.slf4j.LoggerFactory.getLogger(logname);
+	}
+
+	@Override
+	public void log(com.grey.logging.Logger.LEVEL lvl, CharSequence logmsg)
+	{
+		if (!isActive(lvl)) return;
+		String msg = logmsg.toString();
+
+		switch (lvl)
+		{
+			case ERR:
+				extlog.error(msg);
+				break;
+			case WARN:
+				extlog.warn(msg);
+				break;
+			case INFO:
+				extlog.info(msg);
+				break;
+			case TRC:
+				extlog.debug(msg);
+				break;
+			default:
+				extlog.trace(msg);
+				break;
+		}
+	}
+
+	// remove settings that make no sense for this logger
+	private static com.grey.logging.Parameters adjust(com.grey.logging.Parameters params)
+	{
+		params.mode = com.grey.logging.Parameters.MODE_AUDIT;
+		params.pthnam = null;
+		params.strm = null;
+		params.rotfreq = ScheduledTime.FREQ.NEVER;
+		params.maxsize = 0;
+		params.bufsiz = 0;
+		params.flush_interval = 0;
+		return params;
+	}
+}
